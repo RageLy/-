103aa171e8152b3d055fd3f3379a059e9f0ca0eb
rage <974011732@qq.com>
2016-11-30 12:01:10 +0800

    2016-11-30-12-信息修改

--- 居保缴费客户端/change_inf.Designer.cs
+++ 居保缴费客户端/change_inf.Designer.cs
@@ -52,6 +52,8 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -101,6 +103,8 @@
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.label11);
             this.groupBox2.Controls.Add(this.comboBox4);
             this.groupBox2.Controls.Add(this.comboBox3);
             this.groupBox2.Controls.Add(this.label10);
@@ -131,7 +135,7 @@
             // 
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(477, 242);
+            this.comboBox4.Location = new System.Drawing.Point(477, 220);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(121, 20);
             this.comboBox4.TabIndex = 21;
@@ -140,7 +144,7 @@
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(120, 242);
+            this.comboBox3.Location = new System.Drawing.Point(120, 220);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(137, 20);
             this.comboBox3.TabIndex = 20;
@@ -148,7 +152,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(31, 302);
+            this.label10.Location = new System.Drawing.Point(27, 329);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(47, 12);
             this.label10.TabIndex = 19;
@@ -156,7 +160,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(416, 339);
+            this.button3.Location = new System.Drawing.Point(414, 351);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 18;
@@ -167,37 +171,38 @@
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(477, 177);
+            this.comboBox2.Location = new System.Drawing.Point(477, 162);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 20);
             this.comboBox2.TabIndex = 17;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(523, 339);
+            this.button2.Location = new System.Drawing.Point(523, 351);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 16;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(120, 176);
+            this.textBox5.Location = new System.Drawing.Point(120, 162);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(137, 21);
             this.textBox5.TabIndex = 12;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(477, 116);
+            this.textBox4.Location = new System.Drawing.Point(477, 102);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(121, 21);
             this.textBox4.TabIndex = 11;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(120, 116);
+            this.textBox3.Location = new System.Drawing.Point(120, 102);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(137, 21);
             this.textBox3.TabIndex = 10;
@@ -212,7 +217,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(377, 245);
+            this.label9.Location = new System.Drawing.Point(377, 223);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(41, 12);
             this.label9.TabIndex = 8;
@@ -221,7 +226,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(31, 245);
+            this.label8.Location = new System.Drawing.Point(31, 223);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(41, 12);
             this.label8.TabIndex = 7;
@@ -230,7 +235,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(377, 179);
+            this.label7.Location = new System.Drawing.Point(377, 165);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(41, 12);
             this.label7.TabIndex = 6;
@@ -239,7 +244,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(31, 179);
+            this.label6.Location = new System.Drawing.Point(31, 165);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(41, 12);
             this.label6.TabIndex = 5;
@@ -248,7 +253,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(377, 119);
+            this.label5.Location = new System.Drawing.Point(377, 105);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(41, 12);
             this.label5.TabIndex = 4;
@@ -257,7 +262,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 119);
+            this.label4.Location = new System.Drawing.Point(31, 105);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 3;
@@ -275,7 +280,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(377, 52);
+            this.label1.Location = new System.Drawing.Point(377, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 1;
@@ -289,6 +294,22 @@
             this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 0;
             this.label3.Text = "label3";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(31, 284);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 12);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "label11";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(120, 281);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(137, 21);
+            this.textBox6.TabIndex = 24;
             // 
             // change_inf
             // 
@@ -334,5 +355,7 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label11;
     }
 }
--- 居保缴费客户端/change_inf.cs
+++ 居保缴费客户端/change_inf.cs
@@ -35,6 +35,7 @@ namespace 居保缴费客户端
             button3.Text = "下一个";
             button3.Visible = false;
             button3.Enabled = false;
+            textBox6.Enabled = false;
 
             label1.Text = "性别";
             label2.Text = "身份证号码";
@@ -46,6 +47,7 @@ namespace 居保缴费客户端
             label8.Text = "特殊群体";
             label9.Text = "类型";
             label10.Text = "";
+            label11.Text = "是否缴费";
 
             comboBox1.Items.Add("女");
             comboBox1.Items.Add("男");
@@ -58,6 +60,10 @@ namespace 居保缴费客户端
             comboBox4.Items.Add("正常续保");
             comboBox4.Items.Add("新参保");
             comboBox4.Items.Add("中断库");
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
 
             steps[0] = "数据库连接中..."; r_steps[0] = "数据库连接中...";
             steps[1] = "数据库连接成功"; r_steps[1] = "数据库连接失败";
@@ -78,55 +84,40 @@ namespace 居保缴费客户端
             bool results = false;
             string con = ConfigurationManager.ConnectionStrings["connection1"].ConnectionString;
             SqlConnection connection1 = new SqlConnection(con);
-            SqlCommand cmd = new SqlCommand("Query_Inf", connection1);
             try
             {
                 connection1.Open();
-
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@ID", SqlDbType.VarChar, 22).Value = textBox2.Text.Trim();
-                cmd.Parameters.Add("@MARKS1", SqlDbType.Int, 4);
-                cmd.Parameters.Add("@MARKS2", SqlDbType.Int, 4);
-                cmd.Parameters.Add("@MARKS3", SqlDbType.NVarChar, 12);
-                cmd.Parameters.Add("@SEX", SqlDbType.NVarChar, 12);
-                cmd.Parameters.Add("@TESHURENQUN", SqlDbType.NVarChar, 12);
-                cmd.Parameters.Add("@YILIAODAIYU", SqlDbType.NVarChar, 12);
-                cmd.Parameters.Add("@LEIXING", SqlDbType.NVarChar, 12);
-                cmd.Parameters.Add("@BIRTHDAY", SqlDbType.NVarChar, 12);
-                cmd.Parameters.Add("@NAME", SqlDbType.NVarChar, 12);
-                cmd.Parameters.Add("@HANGHAO", SqlDbType.UniqueIdentifier, 14);
-
-
-                cmd.Parameters["@HANGHAO"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@MARKS1"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@MARKS2"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@MARKS3"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@SEX"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@TESHURENQUN"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@YILIAODAIYU"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@LEIXING"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@BIRTHDAY"].Direction = ParameterDirection.Output;
-                cmd.Parameters["@NAME"].Direction = ParameterDirection.Output;
-
-                int s = cmd.ExecuteNonQuery();
-                string[] res = new string[5];
-                string a1 = "";
-                res[0] = cmd.Parameters["@MARKS1"].Value.ToString();
-                res[1] = cmd.Parameters["@MARKS2"].Value.ToString();
-                res[2] = cmd.Parameters["@MARKS3"].Value.ToString();
-
-                if (res[0] == "0")
+                string sqlstring1 = "SELECT * FROM 人员基础信息  WHERE 公民身份号码='"+textBox2.Text.Trim()+"'";
+                // MessageBox.Show(sqlstring1);
+                SqlCommand mycom = new SqlCommand(sqlstring1, connection1);
+                SqlDataReader dr = mycom.ExecuteReader();
+           
+                results = true;
+                if (dr.Read())
                 {
-                    MessageBox.Show("人员库中没有该人员信息,请确认身份证无误\n如果是新参保人员请点击\n菜单中人员信息录入");
-                }
-                else
-                {
-                    guid = new Guid(cmd.Parameters["@HANGHAO"].Value.ToString());
-                    MessageBox.Show(cmd.Parameters["@NAME"].Value.ToString());
-                    textBox1.Text = cmd.Parameters["@NAME"].Value.ToString();
-                    textBox3.Text = textBox2.Text.Trim();
-                    a1 = cmd.Parameters["@SEX"].Value.ToString();
-                    if (a1 == "男")
+                    string[] RESS = new string[15];
+                    guid =new Guid(dr[0].ToString());
+                    RESS[0] = dr[0].ToString();                  //  行号
+                    RESS[1] = dr[1].ToString();                   //个人编号
+                    RESS[2] = dr[2].ToString();                   //单位编号
+                    RESS[3] = dr[3].ToString();                   //单位名称
+                    RESS[4] = dr[4].ToString();                   //身份证号码
+                    RESS[5] = dr[5].ToString();                   //姓名
+                    RESS[6] = dr[6].ToString();                   //特殊人群
+                    RESS[7] = dr[7].ToString();                   //性别
+                    RESS[8] = dr[8].ToString();                   //出生日期
+                    RESS[9] = dr[9].ToString();                   //社会保障卡号
+                    RESS[10] = dr[10].ToString();                   //医疗待遇
+                    RESS[11] = dr[11].ToString();                   //类型
+                    RESS[12] = dr[12].ToString();                   //是否缴费
+                    RESS[13] = dr[13].ToString();                   //身份证问题
+                    textBox1.Text = RESS[5];
+                    textBox3.Text = RESS[4];
+                    textBox4.Text = DateTime.Parse(RESS[8]).ToShortDateString();
+                    textBox5.Text = RESS[9];
+                    textBox6.Text = RESS[12];
+                    label10.Text = "身份证问题：   "+RESS[13];         
+                    if (RESS[7] == "男")
                     {
                         comboBox1.SelectedIndex = 1;
                     }
@@ -135,10 +126,7 @@ namespace 居保缴费客户端
                         comboBox1.SelectedIndex = 0;
                     }
 
-                    textBox4.Text = cmd.Parameters["@BIRTHDAY"].Value.ToString();
-
-                    a1 = cmd.Parameters["@YILIAODAIYU"].Value.ToString().Trim();
-                    if (a1 == "成年人")
+                    if (RESS[10] == "成年人")
                     {
                         comboBox2.SelectedIndex = 0;
                     }
@@ -148,12 +136,28 @@ namespace 居保缴费客户端
                     }
 
 
-                    a1 = cmd.Parameters["@LEIXING"].Value.ToString();
-                    if (a1 == "正常续保")
+                    if (RESS[6] == "非特殊人群")
+                    {
+                        comboBox3.SelectedIndex = 0;
+                    }
+                    else if (RESS[6] == "困难老人")
+                    {
+                        comboBox3.SelectedIndex = 1;
+                    }
+                    else if (RESS[6] == "低保人员")
+                    {
+                        comboBox3.SelectedIndex = 2;
+                    }
+                    else
+                    {
+                        comboBox3.SelectedIndex = 3;
+                    }
+
+                    if (RESS[11] == "正常续保")
                     {
                         comboBox4.SelectedIndex = 0;
                     }
-                    else if (a1 == "新参保")
+                    else if (RESS[11] == "新参保")
                     {
                         comboBox4.SelectedIndex = 1;
                     }
@@ -162,27 +166,88 @@ namespace 居保缴费客户端
                         comboBox4.SelectedIndex = 2;
                     }
 
-                    if (res[2] == "正常")
-                    {
-                        label10.Text = "";
-                        button3.Visible = false;
-                        button3.Enabled = false;
-                    }
-                    else if (res[2] == "存在重复号码")
-                    {
-                        label10.Text = "注意：该身份证存在问题，具体表现为" + res[2] + " 如果信息不符合，点击下一个查看相同身份证的其他人员的信息";
-                        button3.Visible = true;
-                        button3.Enabled = true;
-                    }
-                    else
-                    {
-                        label10.Text = "注意：该身份证存在问题，具体表现为" + res[2];
-                        button3.Visible = false;
-                        button3.Enabled = false;
-                    }
+                }
+                else
+                {
+                    MessageBox.Show("该人员不存在");
+                }
+
+                connection1.Close();
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.ToString());
+            }
+            return results;
+        }
+
+        private bool data_change()        //按照查询的行号修改这个人的基本信息
+        {
+            bool results = false;
+            string con = ConfigurationManager.ConnectionStrings["connection1"].ConnectionString;
+            SqlConnection connection1 = new SqlConnection(con);
+            try
+            {
+                connection1.Open();
+                string sqlstring1 = "UPDATE 人员基础信息 SET 公民身份证号码='"+textBox3.Text.Trim()+"',姓名='"+ textBox1.Text.Trim()
+                    + "',性别='" + comboBox1.Text + "',出生日期='" + textBox4.Text.Trim() + "'社会保障卡号='" + textBox5.Text.Trim() 
+                    + "',医疗待遇类别='" + comboBox2.Text + "',特殊群体='" + comboBox3.Text + "',类型='" + comboBox4.Text
+                    + "', WHERE 行号='" + guid + "'";
+                MessageBox.Show(sqlstring1);
+                SqlCommand mycom = new SqlCommand(sqlstring1, connection1);
+                int s = mycom.ExecuteNonQuery();
+                if(s==0)
+                {
+                    MessageBox.Show("修改失败");
+                }
+                results = true;
+              
+                connection1.Close();
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.ToString());
+            }
+            return results;
+        }
+
+        private bool date_check()         //检查输入的值是否符合规范
+        {
+            bool results = false;
+
+            return results;
+        }
+
+        private bool query_id()    //检查输入的身份证在数据库中是否存在
+        {
+            bool results = false;
+            string con = ConfigurationManager.ConnectionStrings["connection1"].ConnectionString;
+            SqlConnection connection1 = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand("ID_QUERY", connection1);
+            try
+            {
+                connection1.Open();
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@ID", SqlDbType.VarChar, 22).Value = textBox2.Text.Trim();
+                cmd.Parameters.Add("@RES", SqlDbType.Int, 4);
+
+
+
+                cmd.Parameters["@RES"].Direction = ParameterDirection.Output;
+
+
+                int s = cmd.ExecuteNonQuery();
+                string[] res = new string[5];
+                res[0] = cmd.Parameters["@RES"].Value.ToString();
+                if (res[0] == "0")
+                {
                     results = true;
                 }
-                
+                else
+                {
+                    MessageBox.Show("身份证已存在");
+                }
                 connection1.Close();
             }
             catch (Exception e1)
@@ -199,6 +264,15 @@ namespace 居保缴费客户端
             if(res==false)
             {
                 MessageBox.Show("查询失败");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)     //修改个人的用户信息
+        {
+            bool res = data_change();
+            if (res == false)
+            {
+                MessageBox.Show("修改失败");
             }
         }
     }
